666dcd8af64760a296ccce7c1d1fef839b72471c
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2020-06-20 01:01:30 -0400

    Rounding because picsum doens't have something of width 209.99999999999

--- localdashboard/src/components/timeCard.js
+++ localdashboard/src/components/timeCard.js
@@ -17,8 +17,8 @@ class TimeCard extends Component{
     }, 1000)
   }
   render(){
-      var third_width = window.innerWidth/3;
-      var half_height = window.innerHeight/2;
+      var third_width = Math.round(window.innerWidth/3);
+      var half_height = Math.round(window.innerHeight/2);
     return (
     <div style={{margin:"auto", backgroundImage:"url(https://picsum.photos/"+third_width+"/"+half_height+")",height:half_height+"px", width:third_width+"px"}}>
       <h1 id="time">{this.state.time}</h1> 
