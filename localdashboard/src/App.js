825c6dd77c6674b05a4674dfaa7dc10794840d2a
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2020-06-19 20:57:00 -0400

    Changing structure of application

--- localdashboard/src/App.js
+++ localdashboard/src/App.js
@@ -1,32 +1,14 @@
 import React, {Component} from 'react';      
+import NasaCard from './components/nasaCard.js'
 import axios from 'axios';
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={
-      nasa_image:null
-    }
-  }
-  componentDidMount(){
-    this.getNasaImage();
-  }
-  async getNasaImage(){
-    let nasa_response = axios.get("https://api.nasa.gov/planetary/apod?api_key=WSbzw7acACoeokCkenEnI8GgynOR5I4AkDPeix0n")
-    .then((response)=>{
-      this.setState({
-        nasa_image:response.data.url
-      })
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
+  constructor(props){    
+    super(props)
   }
   render(){
     return (
-      <React.Fragment>
-        <img src={this.state.nasa_image}/>
-      </React.Fragment>
+      <NasaCard></NasaCard>
     );
   }
 }
