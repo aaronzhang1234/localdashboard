2e1a0f8201be5c0f1f10b88f56c66719f308e5e0
aaronzhang1234 <aaronzhang.xyz@gmail.com>
2020-07-06 18:38:40 -0400

    Possibly adding Spotify integration

--- localdashboard/src/components/untitledCard.js
+++ localdashboard/src/components/untitledCard.js
@@ -6,7 +6,37 @@ class UntitledCard extends Component{
     super(props);
   }
   componentDidMount(){
+    window.onSpotifyWebPlaybackSDKReady=()=>{
+      const token = 'BQAqIn8p9y0rq6wBCmec3KnTe5o3HvEs9kTaDnVm5kr1nla6ZT2i9rLDN6DVvcxe0XpKHQkLRz7BVkIACTVRiO8N3ghuqtZnPpc7GNYShzC2Xi43vaRMdiNRINvqgO-gvZc7ryPaScZ8SZJWMWRdkQe6sWOnj0nnrmX3Fuh1';
+    const player = new window.Spotify.Player({
+      name: 'Web Playback SDK Quick Start Player',
+      getOAuthToken: cb => { cb(token); }
+    });
+
+    // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+    // Playback status updates
+    player.addListener('player_state_changed', state => { console.log(state); });
+
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+
+    // Connect to the player!
+    player.connect();
+  } 
   }
+
   render(){
     return (
         <React.Fragment>
